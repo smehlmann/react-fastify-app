6099936e93f005aa284244781dc803ebec0f53a1
smehlmann <sandra.mehlmann@gmail.com>
2023-08-30 08:35:28 -0400

    Add files via upload

--- fastifyApp/index.js
+++ fastifyApp/index.js
@@ -4,8 +4,6 @@ import got from 'got'
 import open from 'open'
 import promptSync from 'prompt-sync'
 import { stringify } from 'csv-stringify/sync'
-import fs from 'fs'
-import path from 'path';
 import * as reportGetters from './reports/reportGetters.js';
 import * as statusReport from './reports/statusReport.js';
 import * as assetCountReport from './reports/assetCountReport.js';
@@ -15,13 +13,17 @@ import * as assetCountReportByEmass from './reports/assetCountReportByEmass.js';
 import * as saReportByLabelAndEmass from './reports/saReportByLabelAndEmass.js';
 import * as assetsByCollectionsReport from './reports/assetsByCollectionsReport.js';
 import * as saReportWithMetricsAndVersions from './reports/saReportWithMetricsAndVersions.js';
+import * as stigBenchmarkByResults from './reports/stigBenchmarkByResults.js';
 import * as manageLabels from './labelOps/manageLabels.js';
+import * as tokenUtils from './reports/tokenUtils.js';
 
 const oidcBase = 'https://stigman.nren.navy.mil/auth/realms/np-stigman'
 const apiBase = 'https://stigman.nren.navy.mil/np/api'
 const client_id = 'np-stig-manager'
 const scope =
   'openid stig-manager:collection stig-manager:user stig-manager:stig stig-manager:op stig-manager:stig:read stig-manager:stig:write'
+
+const myTokenUtils = tokenUtils;
 
 const fastify = Fastify({
   logger: true
@@ -87,7 +89,7 @@ async function createLabels(labelInfo) {
   rows = [];
   try {
     console.log('Create labels.');
-    let tokens = await getTokens(oidcBase, client_id, scope);
+    let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
     rows = await manageLabels.addNewLabel(tokens, labelInfo);
     //rows = await manageLabels.addNewLabel(oidcBase, client_id, scope, labelInfo);
     return rows;
@@ -115,55 +117,62 @@ async function run(selection, emassNums) {
   try {
     if (selection == 1) {
       console.log('Run Assets by Collection');
-      let tokens = await getTokens(oidcBase, client_id, scope);
-      rows = await assetsByCollectionsReport.runAssetByCollectionReport(tokens, emassNums);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await assetsByCollectionsReport.runAssetByCollectionReport(myTokenUtils, emassNums);
       return rows;
     }
     else if (selection == 2) {
       console.log('Run Status Report');
-      let tokens = await getTokens(oidcBase, client_id, scope);
-      rows = await statusReport.runStatusReport(tokens, emassNums);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await statusReport.runStatusReport(myTokenUtils, emassNums);
       return rows;
     }
     else if (selection == 3) {
       console.log('Run Asset Count Report');
-      let tokens = await getTokens(oidcBase, client_id, scope);
-      rows = await assetCountReport.runAssetCountReport(tokens, emassNums);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await assetCountReport.runAssetCountReport(myTokenUtils, emassNums);
       return rows;
     }
     else if (selection == 4) {
       console.log('Run SA Report');
-      let tokens = await getTokens(oidcBase, client_id, scope);
-      rows = await saReport.runSAReport(tokens, emassNums);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await saReport.runSAReport(myTokenUtils, emassNums);
       return rows;
     }
     else if (selection == 5) {
       console.log('Run SA Report by Asset')
-      let tokens = await getTokens(oidcBase, client_id, scope);
-      rows = await saReportByAsset.runSAReportByAsset(tokens, emassNums);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await saReportByAsset.runSAReportByAsset(myTokenUtils, emassNums);
       return rows;
     }
     else if (selection == 6) {
       console.log('Run Asset Count Report by EMASS Number')
-      let tokens = await getTokens(oidcBase, client_id, scope);
-      rows = await assetCountReportByEmass.runAssetCountReportByEmass(tokens, emassNums);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await assetCountReportByEmass.runAssetCountReportByEmass(myTokenUtils, emassNums);
       return rows;
     }
     else if (selection == 7) {
       console.log('Run SAReport by Label and EMASS');
-      let tokens = await getTokens(oidcBase, client_id, scope);
-      rows = await saReportByLabelAndEmass.runSAReportByLabelAndEmass(tokens, emassNums);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await saReportByLabelAndEmass.runSAReportByLabelAndEmass(myTokenUtils, emassNums);
       return rows;
     }
     else if (selection == 8) {
       console.log('Run SAReport with Metrics and STIG Benchmark Revisions');
-      let tokens = await getTokens(oidcBase, client_id, scope);
-      rows = await saReportWithMetricsAndVersions.runSAReportWithMetricsAndVersions(tokens, emassNums);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await saReportWithMetricsAndVersions.runSAReportWithMetricsAndVersions(myTokenUtils, emassNums);
+      return rows;
+    }
+    else if (selection == 9) {
+      // run STIG Benchmark by Results
+      console.log('Run STIG Benchmark by Results');
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
+      rows = await stigBenchmarkByResults.runStigBenchmarkByResults(myTokenUtils, emassNums,);
       return rows;
     }
     else if (selection == 102) {
       console.log('Run Delete All Labels');
-      let tokens = await getTokens(oidcBase, client_id, scope);
+      let tokens = await myTokenUtils.getTokens(oidcBase, client_id, scope);
       rows = await manageLabels.deleteAllLabels(tokens);
       return rows;
     }
@@ -176,95 +185,3 @@ async function run(selection, emassNums) {
   }
 }
 
-function wait(ms = 1000) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
-}
-
-async function poll(fn, fnCondition, ms) {
-  let result = await fn()
-  while (!fnCondition(result)) {
-    await wait(ms)
-    result = await fn()
-  }
-  return result
-}
-
-async function getToken(device_code) {
-  try {
-    console.log('Requesting token')
-    const response = await got.post('https://stigman.nren.navy.mil/auth/realms/np-stigman/protocol/openid-connect/token', {
-      //const response = await got.post('https://stigman.nren.navy.mil/auth/realms/np-stigman/protocol/openid-connect/token',{
-      //const response = await got.post('http://localhost:8080/realms/stigman/protocol/openid-connect/token', {
-      //const response = await got.post('https://login.microsoftonline.com/863af28d-88be-4b4d-a58a-d5c40ee1fa22/oauth2/v2.0/token', {
-      form: {
-        grant_type: 'urn:ietf:params:oauth:grant-type:device_code',
-        client_id: 'np-stig-manager',
-        device_code
-      }
-    }).json()
-    return response
-  }
-  catch (e) {
-    console.log(e)
-    return {}
-  }
-}
-
-async function getDeviceCode(url, client_id, scope) {
-  return await got.post(url, {
-    form: {
-      client_id,
-      scope
-    }
-  }).json()
-}
-
-async function getOidcMetadata(url) {
-  return await got.get(`${url}/.well-known/openid-configuration`).json()
-}
-
-async function getMetricsData(accessToken, myUrl) {
-  //console.log("getMetricsData: Requesting data.")
-  return await got.get(myUrl, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  }).json()
-}
-
-async function getTokens(oidcBase, client_id, scope) {
-
-  try {
-
-    const oidcMeta = await getOidcMetadata(oidcBase)
-    if (!oidcMeta.device_authorization_endpoint) {
-      console.log(`Device Authorization grant is not supported by the OIDC Provider`)
-      process.exit(1);
-    }
-    const response = await getDeviceCode(oidcMeta.device_authorization_endpoint, client_id, scope)
-
-    //console.log(response)
-
-    //await new Promise(resolve => setTimeout(resolve, 5000));
-    //console.log(process.argv)
-
-    //open(process.argv[2] === 'complete' ? response.verification_uri_complete : response.verification_uri)
-    open(response.verification_uri_complete)
-
-
-    let fetchToken = () => getToken(response.device_code)
-
-    let validate = result => !!result.access_token
-    let tokens = await poll(fetchToken, validate, response.interval * 1000)
-    console.log(`Got access token from Keycloak`);
-
-    return tokens;
-  }
-  catch (e) {
-    console.log(e);
-  }
-}
-
-export { getTokens }
